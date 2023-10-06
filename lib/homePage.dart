e0367fe310c4449993947b62c6ab78f5cf37ff97
AyeshaFS51 <ayeshasadia51@gmail.com>
2023-10-06 23:50:11 +0600

    Live Test of Mod_9

---
+++ lib/homePage.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String selectedSize = '';
+
+  void _showSnackBar(BuildContext context, String size) {
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Selected Size  $size'),
+      ),
+    );
+  }
+
+  void _selectSize(String size) {
+    setState(() {
+      selectedSize = size;
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Size Selector"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Wrap(
+          spacing: 15,
+          runSpacing: 10,
+          children: [
+            SizedBox(
+              width: 80,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: () {
+                  _selectSize('S');
+                  _showSnackBar(context, 'S');
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: 'S' == selectedSize ? Colors.orange : Colors.grey,
+                ),
+                child: Text('S'),
+              ),
+            ),
+            SizedBox(
+              width: 80,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: () {
+                  _selectSize('M');
+                  _showSnackBar(context, 'M');
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: 'M' == selectedSize ? Colors.orange : Colors.grey,
+                ),
+                child: Text('M'),
+              ),
+            ),
+            SizedBox(
+              width: 80,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: () {
+                  _selectSize('L');
+                  _showSnackBar(context, 'L');
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: 'L' == selectedSize ? Colors.orange : Colors.grey,
+                ),
+                child: Text('L'),
+              ),
+            ),
+            SizedBox(
+              width: 80,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: () {
+                  _selectSize('XL');
+                  _showSnackBar(context, 'L');
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: 'XL' == selectedSize ? Colors.orange : Colors.grey,
+                ),
+                child: Text('XL'),
+              ),
+            ),
+            SizedBox(
+              width: 80,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: () {
+                  _selectSize('XXL');
+                  _showSnackBar(context, 'XXL');
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: 'XXL' == selectedSize ? Colors.orange : Colors.grey,
+                ),
+                child: Text('XXL'),
+              ),
+            ),
+            SizedBox(
+              width: 80,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: () {
+                  _selectSize('XXXL');
+                  _showSnackBar(context, 'XXXL');
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: 'XXXL' == selectedSize ? Colors.orange : Colors.grey,
+                ),
+                child: Text('XXXL'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_live_test/homePage.dart';
 
 void main(){
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const HomeScreenState(),
-      title: "App",
-      theme: ThemeData(
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: const Color(0xFF9e9e9e),
-          ),
-        ),
-      ),
+      home: MyHomePage(),
     );
   }
-
-}
-
-class HomeScreenState extends StatefulWidget{
-  const HomeScreenState({super.key});
-
-  @override
-  State<StatefulWidget> createState() => HomeScreenUI();
-
-}
-
-class HomeScreenUI extends State<HomeScreenState>{
-  String activeOnClick = "null";
-
-  @override
-  Widget build(BuildContext context) {
-    void appSnackBar(context, String message){
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(message))
-      );
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Size Selector"),
-      ),
-      body: Center(
-        child: Wrap(
-          children:[
-            ButtonBar(
-              alignment: MainAxisAlignment.start,
-              children: [
-                ElevatedButton(
-                    onPressed: (){
-                      activeOnClick = "S";
-                      appSnackBar(context, "S");
-                      setState(() {});
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(activeOnClick == "S" ? 0xFFff9800 : 0xFF9e9e9e),
-                    ),
-                    child: const Text("S")
-                ),
-                ElevatedButton(
-                    onPressed: (){
-                      activeOnClick = "M";
-                      appSnackBar(context, "M");
-                      setState(() {});
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(activeOnClick == "M" ? 0xFFff9800 : 0xFF9e9e9e),
-                    ),
-                    child: const Text("M")
-                ),
-                ElevatedButton(
-                    onPressed: (){
-                      activeOnClick = "L";
-                      appSnackBar(context, "L");
-                      setState(() {});
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(activeOnClick == "L" ? 0xFFff9800 : 0xFF9e9e9e),
-                    ),
-                    child: const Text("L")
-                ),
-                ElevatedButton(
-                    onPressed: (){
-                      activeOnClick = "XL";
-                      appSnackBar(context, "XL");
-                      setState(() {});
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(activeOnClick == "XL" ? 0xFFff9800 : 0xFF9e9e9e),
-                    ),
-                    child: const Text("XL")
-                ),
-                ElevatedButton(
-                    onPressed: (){
-                      activeOnClick = "XXL";
-                      appSnackBar(context, "XXL");
-                      setState(() {});
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(activeOnClick == "XXL" ? 0xFFff9800 : 0xFF9e9e9e),
-                    ),
-                    child: const Text("XXL")
-                ),
-                ElevatedButton(
-                    onPressed: (){
-                      activeOnClick = "XXXL";
-                      appSnackBar(context, "XXXL");
-                      setState(() {});
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(activeOnClick == "XXXL" ? 0xFFff9800 : 0xFF9e9e9e),
-                    ),
-                    child: const Text("XXXL")
-                ),
-              ],
-            ),
-          ]
-        ),
-      ),
-    );
-  }
-
 }
